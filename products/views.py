78c96a2bcb0ed9110931d95838335fc33f79d432
Sergii Kostanets <sergiikostanets@gmail.com>
2023-09-15 10:33:45 +0100

    add pagination on products page

--- products/views.py
+++ products/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.core.paginator import Paginator
 
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
-    products = Product.objects.all()
+    # Get all products
+    all_products = Product.objects.all()
+
+    # Create a Paginator instance with a specified number of products per page
+    paginator = Paginator(all_products, 48)
+
+    # Get the current page number from the request's GET parameters
+    page = request.GET.get('page')
+
+    # Get the Page object for the current page
+    products = paginator.get_page(page)
 
     context = {
         'products': products,
